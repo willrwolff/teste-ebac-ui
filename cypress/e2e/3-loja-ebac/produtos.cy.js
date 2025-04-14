ee022759f687b2442fb75c74d1a650f67372a3e4
William Wolffenbuttel <williamrwolff@gmail.com>
2025-04-14 16:21:53 +0900

    listas

---
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -0,0 +1,18 @@
+/// <reference types="cypress"/>
+
+describe('Funcionalidade: Produtos', () => {
+    
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+    })
+
+    it('Deve selecionar um produto da lista', () => {
+        cy.get('.products > .row')
+            //.first()
+           // .eq(2)
+        .contains('Aero Daily Fitness Tee')
+        .click()
+
+        cy.get('#tab-title-description > a').should('exist')
+    });
+});
